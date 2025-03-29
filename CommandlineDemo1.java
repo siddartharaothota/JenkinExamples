9e461b63060175a9483046f278a7ae25c46d769c
Siddartha Rao Thota <siddartharao8@gmail.com>
2025-03-29 12:27:09 +0530

    Update CommandlineDemo1.java

--- CommandlineDemo1.java
+++ CommandlineDemo1.java
@@ -1,7 +1,7 @@
 class CommandlineDemo1{
 	public static void main(String args[]){
-		int n1 = Integer.parseInt(args[0]);
-		int n2 = Integer.parseInt(args[1]);
+		int n1 = args[0];
+		int n2 = args[1];
 
 		System.out.println("name: "+n1);
 		System.out.println("rollNo: "+n2);
